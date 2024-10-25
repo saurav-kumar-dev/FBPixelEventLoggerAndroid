3e2c6a285fe184ffbc7cb672c3ee98ea1ca98f16
webexpert <webexpert.esfera@gmail.com>
2024-10-25 08:03:49 +0530

    Initial Commit

---
+++ app/src/main/java/com/doctor/fbpixeleventloggerandroid/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.doctor.fbpixeleventloggerandroid
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.facebook.appevents.AppEventsLogger
+import java.util.Currency
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        logCustomEvent("HomeScreen_Android")
+    }
+
+    private fun logCustomEvent(eventName: String) {
+        val logger = AppEventsLogger.newLogger(this)
+        logger.logEvent(eventName)
+    }
+}
